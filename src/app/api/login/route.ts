4231e1bac64e04b74ec327adb8bd75edc9edf580
whauzan <whauzan@air.local>
2024-05-16 04:46:51 +0700

    feat: login api

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,48 +1,11 @@
+import bcryptjs from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
 
-import { signIn } from "@/auth";
-import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { getUserByEmail } from "@/lib/db/user";
+
 import { Login, LoginSchema } from "@/schema/AuthSchema";
 
 export async function POST(req: NextRequest) {
-  // try {
-  //   const { email, password } = (await req.json()) as Login;
-  //   const validatedFields = await LoginSchema.safeParse({ email, password });
-
-  //   if (!validatedFields.success) {
-  //     return NextResponse.json({
-  //       message: "Login failed!",
-  //       data: null,
-  //       error: validatedFields.error.flatten().fieldErrors,
-  //     });
-  //   }
-
-  //   const { email: validatedEmail, password: validatedPassword } =
-  //     validatedFields.data;
-
-  //   const existingUser = await getUserByEmail(validatedEmail);
-
-  //   if (!existingUser) {
-  //     return NextResponse.json({
-  //       message: "Login failed!",
-  //       data: null,
-  //       error: {
-  //         email: "Email does not exist!",
-  //       },
-  //     });
-  //   }
-
-  //   return NextResponse.json({
-  //     success: "Logged in successfully!",
-  //     data: { name: existingUser.name, email: existingUser.email },
-  //     error: null,
-  //   });
-  // } catch (error) {
-  //   console.log(error);
-
-  //   return NextResponse.json({ error });
-  // }
-
   try {
     const { email, password } = (await req.json()) as Login;
     const validatedFields = await LoginSchema.safeParse({ email, password });
@@ -57,10 +20,38 @@ export async function POST(req: NextRequest) {
 
     const { email: validatedEmail, password: validatedPassword } =
       validatedFields.data;
-    await signIn("credentials", {
-      email: validatedEmail,
-      password: validatedPassword,
-      redirectTo: DEFAULT_LOGIN_REDIRECT,
+
+    const existingUser = await getUserByEmail(validatedEmail);
+
+    if (!existingUser) {
+      return NextResponse.json({
+        message: "Login failed!",
+        data: null,
+        error: {
+          email: "Email does not exist!",
+        },
+      });
+    }
+
+    const comparePassword = await bcryptjs.compare(
+      validatedPassword,
+      existingUser.password as string,
+    );
+
+    if (!comparePassword) {
+      return NextResponse.json({
+        message: "Login failed!",
+        data: null,
+        error: {
+          password: "Invalid credentials!",
+        },
+      });
+    }
+
+    return NextResponse.json({
+      success: "Logged in successfully!",
+      data: { name: existingUser.name, email: existingUser.email },
+      error: null,
     });
   } catch (error) {
     console.log(error);
